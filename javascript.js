1566fa171ae31d0bef23eaa1f2b7f44e191857e0
Brechoire <brechoire.j@gmail.com>
2016-06-03 12:35:59 +0200

    add style & title

---
+++ javascript.js
@@ -0,0 +1,9 @@
+// Accès au titre
+var titreElt = document.querySelector("h1");
+titreElt.textContent        = "Titre de mon formulaire"; // Ajout du titre
+// Stylisation du titre
+titreElt.style.color        = "#78AB4E";
+titreElt.style.background   = "#404852"; 
+titreElt.style.padding      = "10px";
+titreElt.style.borderRadius = "10px";
+titreElt.style.textAlign    = "center";
