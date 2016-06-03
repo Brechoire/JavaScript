78505ed5e9615db5f74d0bf159e159264fc4881c
Brechoire <brechoire.j@gmail.com>
2016-06-03 15:04:10 +0200

    add color label

--- javascript.js
+++ javascript.js
@@ -7,3 +7,9 @@ titreElt.style.background   = "#404852";
 titreElt.style.padding      = "10px";
 titreElt.style.borderRadius = "10px";
 titreElt.style.textAlign    = "center";
+
+// Couleur sur les label
+var labelElt = document.querySelectorAll("label");
+for (i = 0; i < labelElt.length; i++){
+    labelElt[i].style.color = "#78AB4E";
+}
